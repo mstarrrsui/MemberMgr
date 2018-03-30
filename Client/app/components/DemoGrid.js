802bacb2990f20ec0c1529f9f679dd1b37733596
Mike Starr <mstarr@rsui.com>
2018-03-30 08:53:08 -0400

    demo grid

--- Client/app/components/DemoGrid.js
+++ Client/app/components/DemoGrid.js
@@ -1,64 +1,73 @@
-import React from 'react';
+import React from "react";
 
-import { Grid, GridColumn as Column, GridCellProps, GridCell, GridToolbar, GridDetailRow } from '@progress/kendo-react-grid';
+import {
+  Grid,
+  GridColumn as Column,
+  GridCellProps,
+  GridCell,
+  GridToolbar,
+  GridDetailRow
+} from "@progress/kendo-react-grid";
 
-import { filterBy } from '@progress/kendo-data-query';
+import { filterBy } from "@progress/kendo-data-query";
 //import { sampleProducts } from './sample-products.js';
-import members from '../members.json';
+import members from "../members.json";
 
 class CustomCell extends GridCell {
-    render() {
-        return (
-            <td>
-                <input disabled type="checkbox" checked={this.props.dataItem[this.props.field]} />
-            </td>
-        );
-    }
+  render() {
+    return (
+      <td>
+        <input
+          disabled
+          type="checkbox"
+          checked={this.props.dataItem[this.props.field]}
+        />
+      </td>
+    );
+  }
 }
 
 class DemoGrid extends React.Component {
-    constructor(props) {
-        super(props);
-       
-        this.state = {
-            data: this.GetProducts(null),
-            filter: null
-        };
-        this.filterChange = this.filterChange.bind(this);
-    }
+  constructor(props) {
+    super(props);
 
-    filterChange(event) {
-        this.setState({
-            data: this.GetProducts(event.filter),
-            filter: event.filter
-        });
-    }
+    this.state = {
+      data: this.GetProducts(null),
+      filter: null
+    };
+    this.filterChange = this.filterChange.bind(this);
+  }
 
-    GetProducts(filter) {
-        var data = members.slice();
-        if (filter)
-            return filterBy(data, filter);
-        else
-            return data;
-    }
+  filterChange(event) {
+    this.setState({
+      data: this.GetProducts(event.filter),
+      filter: event.filter
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <Grid
-                    data={this.state.data}
-                    filterable={true}
-                    filter={this.state.filter}
-                    filterChange={this.filterChange}
-                    style={{ maxHeight: '720px', minHeight: '400px' }}
-                >
-                    <Column field="MemberFirstName" title="First Name"  />
-                    <Column field="MemberLastName" title="Last Name"  />
-                    <Column field="StreetAddress" title="Address" />
-                </Grid>
-            </div>
-        );
-    }
+  GetProducts(filter) {
+    var data = members.slice();
+    if (filter) return filterBy(data, filter);
+    else return data;
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid
+          data={this.state.data}
+          filterable={true}
+          filter={this.state.filter}
+          filterChange={this.filterChange}
+          style={{ maxHeight: "720px", minHeight: "400px" }}
+        >
+          <Column field="MemberFirstName" title="First Name" />
+          <Column field="MemberLastName" title="Last Name" />
+          <Column field="StreetAddress" title="Address" />
+        </Grid>
+      </div>
+    );
+  }
 }
 
-module.exports = DemoGrid
+module.exports = DemoGrid;
