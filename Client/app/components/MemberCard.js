3886b31525c0042c3741c90fddac1ed921d4b62f
Mike Starr <mstarr@rsui.com>
2018-05-14 20:52:57 -0400

    updates to cards and return all cards

--- Client/app/components/MemberCard.js
+++ Client/app/components/MemberCard.js
@@ -7,7 +7,15 @@ const MemberCard = ({member}) =>  {
   return (
     <div className="well membercard col-md-3">
         <h4 className="membertitle">{member.MemberLastName + ', ' + member.MemberFirstName}</h4>
-        <div className="memberaddress">{member.StreetAddress}</div>
+        <div className="memberaddress"><strong>{member.StreetAddress}</strong></div>
+        <div className="memberaddress"><strong>{member.City + ', ' + member.State + ' ' + member.Zipcode}</strong></div>
+        <div>{member.MemberEmailAddress}</div>
+        <div>Phone: {member.MemberCellPhone}</div>
+        {member.SpouseLastName ? 
+            <div>Spouse: {member.SpouseLastName + ', ' + member.SpouseFirstName}</div>
+            :
+            <div></div>
+        }
     </div>
   );
 }
